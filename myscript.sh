c2b1042dc50eb56c58ac9f25468e7ecb0ec5bec7
ARB-arb-arailym <aribosh7@gmail.com>
2026-06-04 23:03:38 +0500

    final

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
-echo "Password lenght"
-read length
-chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"
+
+# 1. Greeting
+echo "=== Password Generator ==="
+
+# 2. Request password length
+echo "Input the length of password (For example: 12):"
+read -r length
+
+# Variable with all available characters (like yours)
+chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%"
 password=""
-for ()
+
+# 3. Loop to generate a password of a given length
+for (( i=0; i<$length; i++ ))
+do
+# Select a random character from the chars string
+rand_index=$(( RANDOM % ${#chars} ))
+password+="${chars:$rand_index:1}"
+done
+
+# 4. Output the finished result password
+echo "-------------------------------------"
+echo "Your generated password:"
+echo "$password"
+echo "-------------------------------------"
